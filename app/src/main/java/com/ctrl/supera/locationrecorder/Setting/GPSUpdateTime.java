6ffa0877aba6adf54bc93a37309b66fd2f64c2fc
suxiaocheng <suxiaocheng2010@gmail.com>
2015-11-02 23:34:19 +0800

    add preference dialog, but working error

--- app/src/main/java/com/ctrl/supera/locationrecorder/Setting/GPSUpdateTime.java
+++ app/src/main/java/com/ctrl/supera/locationrecorder/Setting/GPSUpdateTime.java
@@ -1,16 +1,27 @@
 package com.ctrl.supera.locationrecorder.Setting;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.ctrl.supera.locationrecorder.R;
 
 /**
  * Created by Administrator on 2015/11/2.
  */
-public class GPSUpdateTime extends DialogPreference {
-    private int mNewValue;
+public class GPSUpdateTime extends DialogPreference implements SeekBar.OnSeekBarChangeListener {
+    private static final int DEFAULT_VALUE = 0x01;
+
+    private int mCurrentValue;
+    private SeekBar mUpdateTimeSeekBar;
+    private TextView mMinGpsUpdateTimeText;
+
     public GPSUpdateTime(Context context, AttributeSet attrs) {
         super(context, attrs);
 
@@ -22,11 +33,131 @@ public class GPSUpdateTime extends DialogPreference {
     }
 
     @Override
+    public View onCreateDialogView() {
+        View v = super.onCreateDialogView();
+
+        mCurrentValue = getPersistedInt(DEFAULT_VALUE);
+
+        mUpdateTimeSeekBar = (SeekBar) v.findViewById(R.id.seekBarMinGpsUpdateTime);
+        mUpdateTimeSeekBar.setOnSeekBarChangeListener(this);
+        mUpdateTimeSeekBar.setProgress(mCurrentValue);
+
+        mMinGpsUpdateTimeText = (TextView)v.findViewById(R.id.twMinGpsUpdateTime);
+        mMinGpsUpdateTimeText.setText(mCurrentValue);
+
+        return v;
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        mCurrentValue = seekBar.getProgress();
+        mMinGpsUpdateTimeText.setText(mCurrentValue);
+    }
+
+    @Override
     protected void onDialogClosed(boolean positiveResult) {
         // When the user selects "OK", persist the new value
         if (positiveResult) {
-            persistInt(mNewValue);
+            mUpdateTimeSeekBar.getMax();
+            persistInt(mCurrentValue);
         }
     }
 
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        if (restorePersistedValue) {
+            // Restore existing state
+            mCurrentValue = this.getPersistedInt(DEFAULT_VALUE);
+        } else {
+            // Set default state from the XML attribute
+            mCurrentValue = (Integer) defaultValue;
+            persistInt(mCurrentValue);
+        }
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        return a.getInteger(index, DEFAULT_VALUE);
+    }
+
+    private static class SavedState extends BaseSavedState {
+        // Member that holds the setting's value
+        // Change this data type to match the type saved by your Preference
+        int value;
+
+        public SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        public SavedState(Parcel source) {
+            super(source);
+            // Get the current preference's value
+            value = source.readInt();  // Change this to read the appropriate data type
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+            // Write the preference's value
+            dest.writeInt(value);  // Change this to write the appropriate data type
+        }
+
+        // Standard creator object using an instance of this class
+        public static final Parcelable.Creator<SavedState> CREATOR =
+                new Parcelable.Creator<SavedState>() {
+
+                    public SavedState createFromParcel(Parcel in) {
+                        return new SavedState(in);
+                    }
+
+                    public SavedState[] newArray(int size) {
+                        return new SavedState[size];
+                    }
+                };
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        final Parcelable superState = super.onSaveInstanceState();
+        // Check whether this Preference is persistent (continually saved)
+        if (isPersistent()) {
+            // No need to save instance state since it's persistent,
+            // use superclass state
+            return superState;
+        }
+
+        // Create instance of custom BaseSavedState
+        final SavedState myState = new SavedState(superState);
+        // Set the state's value with the class member that holds current
+        // setting value
+        myState.value = mCurrentValue;
+        return myState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        // Check whether we saved the state in onSaveInstanceState
+        if (state == null || !state.getClass().equals(SavedState.class)) {
+            // Didn't save the state, so call superclass
+            super.onRestoreInstanceState(state);
+            return;
+        }
+
+        // Cast state to custom BaseSavedState and pass to superclass
+        SavedState myState = (SavedState) state;
+        super.onRestoreInstanceState(myState.getSuperState());
+
+        // Set this Preference's widget to reflect the restored state
+        mUpdateTimeSeekBar.setProgress(myState.value);
+    }
 }
